e544fd292b48140adc852caef6644d3166a43ee3
Diarndt <diarndt36d@gmail.com>
2022-09-16 15:02:58 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
-function select_ol() {
-  document.getElementById("weekly").innerHTML
+
+const links = [{
+  label: 'Week 01 Notes',
+  url: 'week01.html',
+},
+               {
+                 label: 'Week 02 Notes',
+                 url: 'week02.html',
+               }
+];
+  
+const ol = document.getElementById('weekly');
+  
+links.forEach(link => {
+  let li = document.createElement('li');
+  let a = document.createElement('a');
+  a.setAttribute('href', link.url);
+  a.innerText = link.label;
+  
+  li.appendChild(a);
+  
+  ol.appendChild(li);
+  
+});
